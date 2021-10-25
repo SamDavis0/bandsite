69c25b317f585cdf5f8753c00ab6001a210a17eb
Sam Davis <hsndm16@yahoo.com>
2021-10-25 17:37:02 -0500

    finishing touches

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const socket = require('socket.io')
 const port = process.env.PORT || 3000
 
 //static assets
@@ -10,8 +11,17 @@ app.set('view engine', 'ejs')
 app.use(require('./routes/index'))
 app.use(require('./routes/albums'))
 app.use(require('./routes/feedback'))
+app.use(require('./routes/chat'))
 
-
-app.listen(port,() => {
+const server = app.listen(port,() => {
     console.log(`running on port ${port}`);
+})
+
+let io = socket(server)
+
+io.on('connection', (socket) => {
+    
+    socket.on('postMessage', (msgClient) => {
+        io.emit('updateMessage', msgClient)
+    })
 })
