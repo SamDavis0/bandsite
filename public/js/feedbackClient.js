fefcbc35e15766e223635a48c84339bc4d0da312
Sam Davis <hsndm16@yahoo.com>
2021-10-25 09:08:44 -0500

    close to finished

--- public/js/feedbackClient.js
+++ public/js/feedbackClient.js
@@ -37,10 +37,10 @@ const updateFeedback = (messagesArr) => {
 
         htmlBlock += '     <div class="feedback-item item-list media-list">';
         htmlBlock += '       <div class="feedback-item media">';
-        htmlBlock += '       <div class="media-left"><button class="feedback-delete btn btn-xs btn-danger"><span id="' + key + '" class="glyphicon glyphicon-remove"></span></button></div>';
+        // htmlBlock += '       <div class="media-left"><button class="feedback-delete btn btn-xs btn-danger"><span id="' + key + '" class="glyphicon glyphicon-remove"></span></button></div>';
         htmlBlock += '         <div class="feedback-info media-body">';
         htmlBlock += '           <div class="feedback-head">';
-        htmlBlock += '             <div class="feedback-title">' + item.title + ' <small class="feedback-name label label-info">' + item.name + '</small></div>';
+        htmlBlock += '             <div> <small class="feedback-name label label-info fw-bold fs-5">' + item.name + '</small></div>';
         htmlBlock += '           </div>';
         htmlBlock += '           <div class="feedback-message">' + item.message + '</div>';
         htmlBlock += '         </div>'; 
